dc298373f02efaa6d92a7004384a58741f804508
vidhi94 <ghadiyavidhi607@gmail.com>
2025-09-24 17:15:55 +0530

    hotel

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -19,7 +19,7 @@ const clerkWebhooks = async (req, res) =>{
             _id : data.id,
             email : data.email_addresses[0].email_address,
             username : data.first_name + " " + data.last_name,
-            image : data.image_url,
+            Image : data.image_url,
         }
 
         switch (type) {
